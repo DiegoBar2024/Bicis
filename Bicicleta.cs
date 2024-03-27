371673ce5e01179c58b0d2573e09eaabc75cfbd1
DiegoBar2024 <diego.barboza@correo.ucu.edu.uy>
2024-03-27 14:01:22 -0300

    Le puse un comentario

---
+++ Bicicleta.cs
@@ -0,0 +1,29 @@
+namespace Bicicleta
+{
+    using System;
+    public class Bicicleta
+    {
+        public Bicicleta(string marca, string color, int tamaño, string uso)
+        {
+            // Comentario adicional
+            this.Marca = marca;
+            this.Color = color;
+            this.Tamaño = tamaño;
+            if (uso == "Montaña" || uso == "Ciudad"){
+                this.Uso = uso;
+            }
+            else{
+                this.Uso = "Montaña";
+            }
+        }
+
+        public string Marca { get; }
+
+        public string Color { get; set; }
+
+        public int Tamaño { get; }
+
+        public string Uso { get; set; }
+
+    }
+}
