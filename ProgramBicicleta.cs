c86ec4b986623627624a371d7bc0e6c458987d7c
DiegoBar2024 <diego.barboza@correo.ucu.edu.uy>
2024-03-27 21:33:58 -0300

    Instancié un objeto

---
+++ ProgramBicicleta.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Bicicleta
+{
+    public class ProgramBicicleta
+    {
+        public static void Main(string[] args)
+        {
+            Bicicleta b1 = new Bicicleta("marca1", "azul", 100, "Montaña");
+            Bicicleta b2 = new Bicicleta("marca2", "roja", 120, "Ciudad");
+            Bicicleta b3 = new Bicicleta("marca3", "verde", 200, "Calle");
+
+            Bicicleta miBici = new Bicicleta("miMarca","miColor",100,"miCasa");
+        
+            Console.WriteLine($"Bicicleta 1: {b1.Marca}, {b1.Color} de tamaño {b1.Tamaño} y tipo {b1.Uso}");
+            Console.WriteLine($"Bicicleta 2: {b2.Marca}, {b2.Color} de tamaño {b2.Tamaño} y tipo {b2.Uso}");
+            Console.WriteLine($"Bicicleta 3: {b3.Marca}, {b3.Color} de tamaño {b3.Tamaño} y tipo {b3.Uso}");
+
+        }
+    }
+}
